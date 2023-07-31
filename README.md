d1d739a50296c026e4235ba1164987b9ad2e83ae
Dylan To <dylan.to@hotmail.com>
2023-07-31 21:57:35 +0800

    Docs + update deps

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Embederator
 
-Embeds Discord videos that are larger than 50mb.
+A Cloudflare worker that embeds Discord videos that are larger than 50mb. Demo at [embederator.gummi.workers.dev](https://embederator.gummi.workers.dev/). Deploy your own at [workers.dev](https://workers.cloudflare.com/).
+
+## Usage
+
+The relevant route is `/embed`. It takes two query parameters:
+
+- `v`: The direct URL of the video to embed.
+- `t`: The "thumbnail" of the video (any image).
+
+```example
+https://embederator.gummi.workers.dev/embed?v=https://cdn.discordapp.com/attachments/123/456.mp4&t=https://cdn.discordapp.com/attachments/123/456.jpg
+```
+
+## Development
 
 ```sh
 npm install
@@ -9,4 +22,18 @@ npm run dev
 
 ```sh
 npm run deploy
+```
+
+## How it works
+
+It simply sets the the following HTML metadata. The width and height don't matter, it just needs a value.
+
+```js
+{
+  'og:image': thumbnail,
+  'og:type': 'video.other',
+  'og:video:url': video,
+  'og:video:width': '1280',
+  'og:video:height': '720',
+}
 ```
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,31 @@
 import Layout from '../_layout';
 
-export const HomePage = (props: { messages: string[] }) => {
+export const HomePage = () => {
   return (
     <Layout title="Embederator">
-      <h1>Hello Hono!</h1>
+      <h1>Embederator</h1>
+      <p>
+        A Cloudflare worker that embeds Discord videos that are larger than 50mb. Demo at{' '}
+        <a href="https://embederator.gummi.workers.dev/">embederator.gummi.workers.dev</a>. Deploy your own at{' '}
+        <a href="https://workers.cloudflare.com/">workers.dev</a>.
+      </p>
+      <h2 id="usage">Usage</h2>
+      <p>
+        The relevant route is <code>/embed</code>. It takes two query parameters:
+      </p>
       <ul>
-        {props.messages.map((message) => {
-          return <li>{message}!!</li>;
-        })}
+        <li>
+          <code>v</code>: The direct URL of the video to embed.
+        </li>
+        <li>
+          <code>t</code>: The thumbnail of the video.
+        </li>
       </ul>
+      <pre>
+        <a href="https://embederator.gummi.workers.dev/embed?v=https://cdn.discordapp.com/attachments/123/456.mp4&amp;t=https://cdn.discordapp.com/attachments/123/456.jpg">
+          https://embederator.gummi.workers.dev/embed?v=https://cdn.discordapp.com/attachments/123/456.mp4&amp;t=https://cdn.discordapp.com/attachments/123/456.jpg
+        </a>
+      </pre>
     </Layout>
   );
 };
